9da05af565870582e1179fa9e849a5168f73125b
Ezo Saleh <ezo@the-experimenters.com>
2014-07-10 12:55:45 +0100

    Can almost build www directly.

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,7 +5,9 @@
 var gulp = require('gulp'),
     browserify = require('browserify'),
     jshint = require('gulp-jshint'),
-    source = require('vinyl-source-stream');
+    notify = require('gulp-notify'),
+    source = require('vinyl-source-stream'),
+    del = require('del');
 
 var paths = {
   src: './src/**/*',
@@ -13,29 +15,58 @@ var paths = {
   js: ['./src/js/**/*.js'],
   jsSrcFile: './src/js/main.js',
   jsBuildFile: 'browserify-main.js',
-  jsBuildFolder: './src/build/js/'
+  build: './src/build'
 };
+
+gulp.task('clean', function(cb) {
+  del([paths.build], cb);
+});
 
 gulp.task('jshint', function () {
   return gulp.src(paths.js)
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'));
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('default'))
+    .pipe(notify({ message: 'jshint task complete' }));
 });
 
 gulp.task('browserify', function() {
-  console.log('paths.jsBuildFolder', paths.jsBuildFolder);
   return browserify(paths.jsSrcFile)
     .bundle()
     .pipe(source(paths.jsBuildFile))
-    .pipe(gulp.dest(paths.jsBuildFolder));
+    .pipe(gulp.dest(paths.build + '/js/'))
+    .pipe(notify({ message: 'browserify task complete' }));
 });
 
-gulp.task('www', function () {
-  return gulp.src(paths.src)
-    .pipe(gulp.dest(paths.www));
+gulp.task('img', function() {
+  return gulp.src('./src/img/**/*')
+    .pipe(gulp.dest(paths.build + '/img'))
+    .pipe(notify({ message: 'img task complete' }));
 });
 
-// gulp.task('build', ['jshint', 'browserify', 'www']);
-gulp.task('build', ['jshint', 'browserify']);
+gulp.task('css', function() {
+  return gulp.src('./src/css/**/*')
+    .pipe(gulp.dest(paths.build + '/css'))
+    .pipe(notify({ message: 'css task complete' }));
+});
+
+gulp.task('lib', function() {
+  return gulp.src('./src/lib/**/*')
+    .pipe(gulp.dest(paths.build + '/lib'))
+    .pipe(notify({ message: 'lib task complete' }));
+});
+
+gulp.task('static', function() {
+  return gulp.src('./src/index.html')
+    .pipe(gulp.dest(paths.build))
+    .pipe(notify({ message: 'static task complete' }));
+});
+
+gulp.task('www', ['clean', 'jshint', 'browserify', 'img', 'css', 'lib', 'static'], function() {
+  return gulp.src('./src/build/**/*')
+    .pipe(gulp.dest(paths.www))
+    .pipe(notify({ message: 'cordova www create task complete' }));
+});
+
+gulp.task('build', ['www']);
 
 gulp.task('default', ['build']);
