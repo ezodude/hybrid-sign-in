8b15eddb9f2f34884874e92c05afa4a80cff2bec
Ezo Saleh <ezo@the-experimenters.com>
2014-07-15 07:59:16 +0100

    Simplify builds.

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,68 +5,61 @@
 var gulp = require('gulp'),
     browserify = require('browserify'),
     jshint = require('gulp-jshint'),
-    notify = require('gulp-notify'),
     source = require('vinyl-source-stream'),
-    del = require('del');
+    execSync = require('exec-sync');
 
 var paths = {
   src: './src/**/*',
   www: './www/',
   js: ['./src/js/**/*.js'],
   jsSrcFile: './src/js/main.js',
-  jsBuildFile: 'browserify-main.js',
-  build: './src/build'
+  jsBuildFile: 'browserify-main.js'
 };
 
 gulp.task('clean', function(cb) {
-  del([paths.build], cb);
+  execSync('rm -rf ./www');
+  cb();
 });
 
-gulp.task('jshint', function () {
-  return gulp.src(paths.js)
+gulp.task('jshint', function (cb) {
+  gulp.src(paths.js)
     .pipe(jshint('.jshintrc'))
-    .pipe(jshint.reporter('default'))
-    .pipe(notify({ message: 'jshint task complete' }));
+    .pipe(jshint.reporter('default'));
+  cb();
 });
 
-gulp.task('browserify', function() {
-  return browserify(paths.jsSrcFile)
+gulp.task('browserify', function(cb) {
+  browserify(paths.jsSrcFile)
     .bundle()
     .pipe(source(paths.jsBuildFile))
-    .pipe(gulp.dest(paths.build + '/js/'))
-    .pipe(notify({ message: 'browserify task complete' }));
+    .pipe(gulp.dest(paths.www + '/js/'));
+  cb();
 });
 
-gulp.task('img', function() {
-  return gulp.src('./src/img/**/*')
-    .pipe(gulp.dest(paths.build + '/img'))
-    .pipe(notify({ message: 'img task complete' }));
+gulp.task('img', function(cb) {
+  gulp.src('./src/img/**/*')
+    .pipe(gulp.dest(paths.www + '/img'));
+  cb();
 });
 
-gulp.task('css', function() {
-  return gulp.src('./src/css/**/*')
-    .pipe(gulp.dest(paths.build + '/css'))
-    .pipe(notify({ message: 'css task complete' }));
+gulp.task('css', function(cb) {
+  gulp.src('./src/css/**/*')
+    .pipe(gulp.dest(paths.www + '/css'));
+  cb();
 });
 
-gulp.task('lib', function() {
-  return gulp.src('./src/lib/**/*')
-    .pipe(gulp.dest(paths.build + '/lib'))
-    .pipe(notify({ message: 'lib task complete' }));
+gulp.task('lib', function(cb) {
+  gulp.src('./src/lib/**/*')
+    .pipe(gulp.dest(paths.www + '/lib'));
+  cb();
 });
 
-gulp.task('static', function() {
-  return gulp.src('./src/index.html')
-    .pipe(gulp.dest(paths.build))
-    .pipe(notify({ message: 'static task complete' }));
+gulp.task('static', function(cb) {
+  gulp.src('./src/index.html')
+    .pipe(gulp.dest(paths.www));
+  cb();
 });
 
-gulp.task('www', ['clean', 'jshint', 'browserify', 'img', 'css', 'lib', 'static'], function() {
-  return gulp.src('./src/build/**/*')
-    .pipe(gulp.dest(paths.www))
-    .pipe(notify({ message: 'cordova www create task complete' }));
-});
-
-gulp.task('build', ['www']);
+gulp.task('build', ['clean', 'jshint', 'browserify', 'static', 'img', 'css', 'lib']);
 
 gulp.task('default', ['build']);
